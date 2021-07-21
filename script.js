5086b9f83ba06f4e3ef08e332f804497ed857017
Daiane Silva <daianemsilva09@gmail.com>
2021-07-21 18:07:47 -0300

    aula 1

---
+++ script.js
@@ -0,0 +1,11 @@
+var nome = "Dai";
+var idade = 29;
+var idade2 = 10;
+var frase = "Japão melhor tipo"
+//alert("Meu nome " + nome);
+//alert(idade + idade2);
+console.log("Meu nome " + nome);
+console.log(idade + idade2);
+console.log(frase);
+console.log(frase.toLowerCase());
+alert(frase.replace("Japão","Brasil"));
